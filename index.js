779330908443b9d9928d28f8c1bad44c66c2a558
PaddeCraft <70023807+PaddeCraft@users.noreply.github.com>
2023-02-21 19:14:33 +0100

    Revert "Revert merge "Added new design for GUI, for better visualisation (Not finished)", will remerge when finished and usable"

--- index.js
+++ index.js
@@ -19,12 +19,16 @@ function parse_xml_by_event(e, callback) {
 }
 
 function get_scene_banks_by_xml(xml) {
+    // from file get group scenes, where the bank & scenes are located
     var scene_banks = xml.querySelector("SCENES");
-    var scene_banks_array = [];
+    var scene_banks_array = [];// List of all banks with containing scenes
+    // iterate through the groups
     for (var i = 0; i < scene_banks.children.length; i++) {
         var bank = scene_banks.children[i];
+        // if the group is a bank:
         if (bank.tagName == "BANK") {
             var bank_scenes = [];
+            // iterate trough all scenes in the bank an add them to bank_scenes
             Array.from(bank.children).forEach(function (scene) {
                 bank_scenes.push({
                     uuid: scene.getAttribute("DASUID"),
@@ -32,6 +36,7 @@ function get_scene_banks_by_xml(xml) {
                     element: scene,
                 });
             });
+            // Add the final bank to scene_banks_array
             scene_banks_array.push({
                 name: bank.getAttribute("NAME"),
                 uuid: bank.getAttribute("DASUID"),
@@ -39,7 +44,6 @@ function get_scene_banks_by_xml(xml) {
             });
         }
     }
-
     return scene_banks_array;
 }
 
@@ -59,15 +63,27 @@ function uuidv4() {
 }
 
 function shift_scene(uuid_src, bank_uuid_dest) {
+    // if bank_uuid_dest does not exist, stop method
     if (!bank_uuid_dest) return;
+    //get GUI Object
+    let dest_bank_html = document.getElementById("dest-bank-id-prefix-" + bank_uuid_dest);
     log("Shifting scene " + uuid_src + " to bank " + bank_uuid_dest);
-
+    // get scene content
     var scene_bank = dest_file_xml.querySelector(
         "BANK[DASUID='" + bank_uuid_dest + "']"
     ).innerHTML;
+    // set new scene content
     var scene = source_file_xml.querySelector(
         "SCENE[DASUID='" + uuid_src + "']"
     );
+    let sceneObj = document.createElement("div");
+    sceneObj.classList.add("singleScene");
+    sceneObj.innerHTML = `
+        <h5>${scene.getAttribute("NAME")}</h5>
+    <div class="sceneMover" onclick="click_moveDestScene('${uuid_src}')">
+        X──
+    </div>`
+    dest_bank_html.appendChild(sceneObj);
     dest_file_xml = new DOMParser().parseFromString(
         dest_file_xml.documentElement.outerHTML.replace(
             scene_bank,
@@ -76,6 +92,9 @@ function shift_scene(uuid_src, bank_uuid_dest) {
         "text/xml"
     );
 }
+/*Array.from(document.getElementsByClassName("tooltip")).forEach((e)=>{
+    
+});*/
 
 function download(filename, text) {
     // https://stackoverflow.com/a/3665147
@@ -96,7 +115,28 @@ function download(filename, text) {
 
     document.body.removeChild(element);
 }
+const overlay = document.getElementById("overlay");
+let mousepos;
+document.addEventListener("mousemove",(e)=>{mousepos = {
+    x: e.pageX,
+    y: e.pageY
+}});
+function click_moveSrcScene(uuid){
+    overlay.style.display = "block";
+    overlay.children[0].style.top = mousepos.y + "px";
+    overlay.children[0].style.left = mousepos.x + "px";
+    overlay.children[0].setAttribute("data-overUUID",uuid);
+}
+function moveSrcScene(bank_uuid){
+    let uuid = overlay.children[0].getAttribute("data-overUUID");
+    cancel_shift();
+    shift_scene(uuid,bank_uuid);
 
+}
+function cancel_shift(){
+    overlay.style.display = "none";
+    overlay.children[0].removeAttribute("data-overUUID");
+}
 $("#source-file-inp").change(function (e) {
     var invalid = document.getElementById("source-file-invalid");
 
@@ -112,11 +152,25 @@ $("#source-file-inp").change(function (e) {
             console.log(scene_banks);
 
             var evt_listeners = [];
-
+            // go through every bank
             scene_banks.forEach(function (scbank) {
                 var scenes_html = "";
-
+                let bankobj = document.createElement("div");
+                bankobj.classList.add("bankSlot");
+                bankobj.innerHTML = `
+                    <h4>${scbank.name}</h4>
+                `
+                document.getElementById("src-scenes").appendChild(bankobj);
+                // go through every scene in the scbank
                 scbank.scenes.forEach(function (scene) {
+                    let sceneObj = document.createElement("div");
+                    sceneObj.classList.add("singleScene");
+                    sceneObj.innerHTML = `
+                        <h5>${scene.name}</h5>
+                    <div class="sceneMover" onclick="click_moveSrcScene('${scene.uuid}')">
+                        ──►
+                    </div>`
+                    bankobj.appendChild(sceneObj);
                     var sc_id = uuidv4();
                     var dest_bank_id = uuidv4();
                     evt_listeners.push({
@@ -146,8 +200,9 @@ $("#source-file-inp").change(function (e) {
             evt_listeners.forEach(function (listener) {
                 $("#" + listener.id).click(listener.f);
             });
-        } catch {
+        } catch (e) {
             log("Error while loading scene banks from source");
+            log(e);
             invalid.style.display = "block";
         }
     });
@@ -167,11 +222,39 @@ $("#dest-file-inp").change(function (e) {
                 "Loaded " + scene_banks.length + " scene banks from destination"
             );
 
+            scene_banks.forEach(function (scbank) {
+                var scenes_html = "";
+                let bankobj = document.createElement("div");
+                bankobj.classList.add("bankSlot");
+                bankobj.id = "dest-bank-id-prefix-" + scbank.uuid;
+                bankobj.innerHTML = `
+                    <h4>${scbank.name}</h4>
+                `
+                document.getElementById("dest-scenes").appendChild(bankobj);
+                let bank = document.createElement("div");
+                bank.classList.add("banksel_item");
+                bank.innerText = scbank.name;
+                bank.onclick = ()=>{
+                    moveSrcScene(scbank.uuid);
+                };
+                document.getElementById("banksel_menu").appendChild(bank);
+                // go through every scene in the scbank
+                scbank.scenes.forEach(function (scene) {
+                    let sceneObj = document.createElement("div");
+                    sceneObj.classList.add("singleScene");
+                    sceneObj.innerHTML = `
+                        <h5>${scene.name}</h5>
+                    <div class="sceneMover" onclick="click_moveDestScene('${scene.uuid}')">
+                        X──
+                    </div>`
+                    bankobj.appendChild(sceneObj);
+
+                });
+            });
             var select_options_html = "";
             scene_banks.forEach(function (scbank) {
                 select_options_html += `<option value="${scbank.uuid}">${scbank.name}</option>`;
             });
-
             document
                 .querySelectorAll(".dest-scene-bank-dropdown")
                 .forEach(function (ele) {
